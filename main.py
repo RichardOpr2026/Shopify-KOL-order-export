2418a1d857149ce079933dd51d2bfddb8aef5b7f
RichardOpr2026 <openroadrichard@gmail.com>
2026-06-15 09:39:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import requests
+import os
 
-print("Hello Shopify Agent")
+SHOPIFY_STORE = os.getenv("SHOPIFY_STORE")
+SHOPIFY_TOKEN = os.getenv("SHOPIFY_TOKEN")
+
+url = f"https://{SHOPIFY_STORE}/admin/api/2025-01/orders.json"
+
+headers = {
+    "X-Shopify-Access-Token": SHOPIFY_TOKEN
+}
+
+response = requests.get(
+    url,
+    headers=headers,
+    params={
+        "status": "any",
+        "limit": 5
+    }
+)
+
+print("Store:", SHOPIFY_STORE)
+print("Status:", response.status_code)
+print(response.text[:1000])
