d084269bf61eb39109fd7d56a7dfd7c1eb854339
RichardOpr2026 <openroadrichard@gmail.com>
2026-06-16 06:31:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -248,7 +248,7 @@ def main():
     print("BITABLE_APP_TOKEN =", BITABLE_APP_TOKEN)
     print("SKU_TABLE_ID =", SKU_TABLE_ID)
     sku_records = get_all_records(token, BITABLE_APP_TOKEN, SKU_TABLE_ID)
-    target_records = get_all_records(token, target_app_token, target_table_id)
+    target_records = []
 
     kol_code_map = build_kol_code_map(kol_records)
     sku_map = build_sku_map(sku_records)
